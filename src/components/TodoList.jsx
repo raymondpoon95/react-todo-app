9f20bc7c80c7d6d1c1f33ef5aca86cf2fd0b016b
Raymond Poon <raymondpoon95@hotmail.co.uk>
2021-08-27 01:10:46 +0100

    inital commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+function TodoList() {
+  let initalTodo;
+
+  if(localStorage.getItem('todos') === null) {
+    initalTodo = [];
+  } else {
+    initalTodo = JSON.parse(localStorage.getItem('todos'));
+  }
+
+
+  const [todos, setTodos] = useState(initalTodo);
+
+  const addTodo = (todo) => {
+    if(!todo.text || /^\s*$/.test(todo.text)) return;
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    updateLocalStorage(newTodos);
+  }
+
+  const updateTodo = (todoId, newValue) => {
+    if(!newValue.text || /^\s*$/.test(newValue.text)) return;
+
+    const updatedTodos = todos.map((item) => item.id === todoId ? newValue : item);
+
+    setTodos(updatedTodos);
+    updateLocalStorage(updatedTodos);
+  }
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removeArr);
+    updateLocalStorage(removeArr);
+  }
+
+  const completeTodo = (id) => { 
+    let updatedTodos = todos.map(todo => {
+      if(todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+    updateLocalStorage(updatedTodos);
+  }
+
+  const updateLocalStorage = data => {
+    localStorage.setItem('todos', JSON.stringify(data));
+  }
+
+  return (
+    <div>
+      <h1>To do List</h1>
+      <TodoForm 
+        onSubmit={addTodo}
+      />
+      <Todo 
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </div>
+  ) 
+}
+
+export default TodoList;
